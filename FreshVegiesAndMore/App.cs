198d83e6c0b8eb36c3565120a07a6a9e7b21a0ae
paramjit singh <sparamjit92@gmail.com>
2023-10-24 17:57:57 +0530

    Initial setup: added folders, NuGets etc

---
+++ FreshVegiesAndMore/App.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace FreshVegiesAndMore
+{
+    public class App : Application
+    {
+        public App()
+        {
+            MainPage = new AppShell();
+        }
+    }
+}
--- FreshVegiesAndMore/MauiProgram.cs
+++ FreshVegiesAndMore/MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using CommunityToolkit.Maui.Markup;
+using Microsoft.Extensions.Logging;
 
 namespace FreshVegiesAndMore
 {
@@ -9,17 +11,73 @@ namespace FreshVegiesAndMore
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                 .UseMauiCommunityToolkit()
+                .UseMauiCommunityToolkitMarkup()
+                 .ConfigureViews()
+                .ConfigureViewModels()
+                .ConfigureServices()
+                .ConfigureHandlers()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
 
+            builder.Services.AddSingleton<AppShell>();
+
 #if DEBUG
     		builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
+        }
+
+        public static MauiAppBuilder ConfigureViews(this MauiAppBuilder builder)
+        {
+
+            //account Views
+           
+
+            // other views
+           
+
+            return builder;
+        }
+
+        public static MauiAppBuilder ConfigureViewModels(this MauiAppBuilder builder)
+        {
+            // account viewmodels
+            
+
+            // other viewmodels
+          
+
+            return builder;
+        }
+
+        public static MauiAppBuilder ConfigureServices(this MauiAppBuilder builder)
+        {
+            //App Services
+         
+           
+
+            // Other Services
+            builder.Services.AddSingleton<IConnectivity>(Connectivity.Current);
+            return builder;
+
+
+        }
+
+        public static MauiAppBuilder ConfigureHandlers(this MauiAppBuilder builder)
+        {
+            return builder.ConfigureMauiHandlers(handlers =>
+            {
+
+
+
+            });
+
+
         }
     }
 }
