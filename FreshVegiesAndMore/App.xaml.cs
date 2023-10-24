6ce388e2689c55e4137837caa4950d06407d4cc1
paramjit singh <sparamjit92@gmail.com>
2023-10-24 17:40:31 +0530

    Add project files.

---
+++ FreshVegiesAndMore/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FreshVegiesAndMore
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ FreshVegiesAndMore/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FreshVegiesAndMore
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
